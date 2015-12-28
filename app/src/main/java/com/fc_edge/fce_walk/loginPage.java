a4e4b7e74e1bdfe08d0a03c77622d574df422b83
Mike <michaeloloughlint@gmail.com>
2015-12-27 20:08:52 -0500

    Added some stuff...

--- app/src/main/java/com/fc_edge/fce_walk/loginPage.java
+++ app/src/main/java/com/fc_edge/fce_walk/loginPage.java
@@ -4,21 +4,55 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.Map;
 
 public class loginPage extends AppCompatActivity {
     private EditText email, password;
     private Button login;
     private RequestQueue requestQueue;
-    private static final String URL = "";
+    private static final String URL = "http://stage.fc-edge.com/user_control.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+        email = (EditText) findViewById(R.id.email);
+        password = (EditText) findViewById(R.id.password);
+        login = (Button) findViewById(R.id.login);
+        requestQueue = Volley.newRequestQueue(this);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                request = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }){
+                    @Override
+                    protected Map<String, String> getParams() throws AuthFailureError {
+                        return super.getParams();
+                    }
+                };
+            }
+        });
     }
 
     @Override
